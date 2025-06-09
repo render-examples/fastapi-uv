557ac479b6f9cb680d3803d3372fe321327f8fa9
Max Eshleman <max@render.com>
2025-06-09 08:57:52 -0600

    prune uv cache after build (#1)

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 # sync the dependecies into the Render cache
 uv sync --locked --no-dev
+
+# prune the uv cache so it doesn't grow indefinitely
+# https://docs.astral.sh/uv/concepts/cache/#caching-in-continuous-integration
+uv cache prune --ci
